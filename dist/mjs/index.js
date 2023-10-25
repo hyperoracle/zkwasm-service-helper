5e3d2da12ee79fffc390108f4f5e90edf535c5b9
MuRong <hi@mrong.me>
2023-10-25 20:57:41 +0800

    refactor: remove `prepare` script and add `files` in package.json

--- dist/mjs/index.js
+++
@@ -1,4 +0,0 @@
-import { ZkWasmUtil } from "./helper/util.js";
-import { ZkWasmServiceEndpoint } from "./helper/endpoint.js";
-import { ZkWasmServiceHelper } from "./helper/task.js";
-export { ZkWasmServiceEndpoint, ZkWasmServiceHelper, ZkWasmUtil, };
